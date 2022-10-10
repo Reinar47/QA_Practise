313a02f5486dc36201cf7bd62c72f152cf85b352
Igor K <reinar4747@gmail.com>
2022-10-10 13:39:59 +0300

    2 new br hello 1

--- src/Git_test.java
+++ src/Git_test.java
@@ -2,5 +2,7 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("1 hello");
         System.out.println("1 hello");
+        System.out.println(" 2 Br 1 hello");
+
     }
 }
